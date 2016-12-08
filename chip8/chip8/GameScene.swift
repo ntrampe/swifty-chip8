8a3513c9db277aea745ca457a71b3d0e99fc7950
Nicholas Trampe <nftn83@mst.edu>
2016-12-08 07:49:46 -0600

    Using sprite kit to draw a simple grid (might want to consider using metal down the road)

--- chip8/chip8/GameScene.swift
+++ chip8/chip8/GameScene.swift
@@ -11,41 +11,68 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    override func didMove(to view: SKView) {
-        
+  var graphics = [SKShapeNode?](repeating: nil, count: 64 * 32) 
+  
+  override func didMove(to view: SKView) {
+    
+    initializeGrid(size: view.frame.size)
+  }
+  
+  func initializeGrid(size: CGSize) {
+    let minds = min(size.width, size.height)
+    let pixelSize = CGSize(width: minds / 64, height: minds / 32)
+    
+    for i in 0...graphics.count-1 {
+      guard let node = graphics[i] else {
+        continue
+      }
+      
+      node.removeFromParent()
     }
     
-    
-    func touchDown(atPoint pos : CGPoint) {
-        
+    for x in 0...63 {
+      for y in 0...31 {
+        let xConverted = (CGFloat(x) * pixelSize.width)
+        let yConverted = size.height - (CGFloat(y) * pixelSize.height)
+        let shape = SKShapeNode(rect: CGRect(x: xConverted, y: yConverted, width: pixelSize.width, height: pixelSize.height))
+        shape.fillColor = UIColor.white
+        shape.strokeColor = UIColor.clear
+        self.addChild(shape)
+        self.graphics[x + (64 * y)] = shape
+      }
     }
+  }
+  
+  func touchDown(atPoint pos : CGPoint) {
     
-    func touchMoved(toPoint pos : CGPoint) {
-        
-    }
+  }
+  
+  func touchMoved(toPoint pos : CGPoint) {
     
-    func touchUp(atPoint pos : CGPoint) {
-        
-    }
+  }
+  
+  func touchUp(atPoint pos : CGPoint) {
     
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {  
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-    }
+  }
+  
+  override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {  
+    for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+  }
+  
+  override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+    for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+  }
+  
+  override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+    for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+  }
+  
+  override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+    for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+  }
+  
+  
+  override func update(_ currentTime: TimeInterval) {
+    // Called before each frame is rendered
+  }
 }
--- chip8/chip8/GameViewController.swift
+++ chip8/chip8/GameViewController.swift
@@ -12,44 +12,49 @@ import GameplayKit
 
 class GameViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
-            
-            view.ignoresSiblingOrder = true
-            
-            view.showsFPS = true
-            view.showsNodeCount = true
-        }
+  var scene: GameScene! = nil
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    if let view = self.view as! SKView? {
+      scene = GameScene(size: view.frame.size)
+      scene.scaleMode = .aspectFill
+      
+      view.presentScene(scene)
+      view.ignoresSiblingOrder = true
+      
+      view.showsFPS = true
+      view.showsNodeCount = true
     }
-
-    override var shouldAutorotate: Bool {
-        return true
+  }
+  
+  override var shouldAutorotate: Bool {
+    return true
+  }
+  
+  override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+    coordinator.animate(alongsideTransition: { (context) in
+      
+    }) { (context) in
+      self.scene.initializeGrid(size: size)
     }
-
-    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
-        }
+  }
+  
+  override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+    if UIDevice.current.userInterfaceIdiom == .phone {
+      return .allButUpsideDown
+    } else {
+      return .all
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
-    }
-
-    override var prefersStatusBarHidden: Bool {
-        return true
-    }
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Release any cached data, images, etc that aren't in use.
+  }
+  
+  override var prefersStatusBarHidden: Bool {
+    return true
+  }
 }
